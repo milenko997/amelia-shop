812796ead9658539059656243fbb7eb41b2b29c5
Milenko Ilic <milenko.ilic997@gmail.com>
2026-05-25 17:24:56 +0200

    Settuping project

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+const path          = require( 'path' );
+
+module.exports = {
+	...defaultConfig,
+
+	entry: {
+		// Theme JS + SCSS (SCSS imported inside main.js)
+		main: path.resolve( __dirname, 'src/js/main.js' ),
+
+		// Gutenberg blocks
+		'blocks/hero/index':           path.resolve( __dirname, 'src/blocks/hero/index.js' ),
+		'blocks/category-grid/index':  path.resolve( __dirname, 'src/blocks/category-grid/index.js' ),
+		'blocks/features-strip/index': path.resolve( __dirname, 'src/blocks/features-strip/index.js' ),
+		'blocks/newsletter/index':     path.resolve( __dirname, 'src/blocks/newsletter/index.js' ),
+	},
+
+	output: {
+		...defaultConfig.output,
+		path:     path.resolve( __dirname, 'build' ),
+		filename: '[name].js',
+	},
+};
