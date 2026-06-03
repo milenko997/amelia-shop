fd7c728c3c9d1f80009c75132f81a0239d7c35a3
Milenko Ilic <milenko.ilic997@gmail.com>
2026-06-03 16:13:38 +0200

    Redesigning checkout page

--- functions.php
+++ functions.php
@@ -159,6 +159,7 @@ add_action( 'widgets_init', 'amelia_sidebars' );
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper',     10 );
 remove_action( 'woocommerce_after_main_content',  'woocommerce_output_content_wrapper_end', 10 );
 remove_action( 'woocommerce_sidebar',             'woocommerce_get_sidebar',                10 );
+remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form',      10 );
 
 function amelia_wc_before_content() {
 	if ( is_cart() || is_checkout() || is_account_page() ) {
