119ec8c68d41e1d47d57b3ec6827de2d9cd2f3f1
Milenko Ilic <milenko.ilic997@gmail.com>
2026-06-09 16:12:32 +0200

    Removing page title

--- page.php
+++ page.php
@@ -11,9 +11,6 @@ $is_wc_page = function_exists( 'is_cart' ) && ( is_cart() || is_checkout() || is
 	<main id="main" class="site-main">
 		<?php while ( have_posts() ) : the_post(); ?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<div class="page-header container" style="max-width:900px;padding:3rem 1.5rem 1.5rem;">
-					<h1 class="page-title"><?php the_title(); ?></h1>
-				</div>
 				<div class="page-content">
 					<?php the_content(); ?>
 				</div>
